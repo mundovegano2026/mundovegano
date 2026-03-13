69385c1d6f27df020d00d6bdd08471e2ccc6dad1
JQEstrada <joaoestrada@msn.com>
2026-03-13 23:32:48 +0000

    docker fix

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -6,6 +6,12 @@ composer install --no-dev --working-dir=/var/www/html
 echo "Installing Node dependencies & building frontend..."
 npm install && npm run prod
 
+echo "Fixing storage permissions..."
+chmod -R 775 /var/www/html/storage
+chmod -R 775 /var/www/html/bootstrap/cache
+chown -R nginx:nginx /var/www/html/storage
+chown -R nginx:nginx /var/www/html/bootstrap/cache
+
 echo "Caching config..."
 php artisan config:cache
 
