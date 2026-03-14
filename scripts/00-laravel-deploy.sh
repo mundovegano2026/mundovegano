888ee0d4652275bcace2352432d5c2b2ebd1274e
JQEstrada <joaoestrada@msn.com>
2026-03-14 00:10:58 +0000

    permissions fix

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,6 +3,13 @@
 echo "Clearing cache..."
 rm -rf /var/www/html/bootstrap/cache/*.php
 
+echo "Fixing permissions..."
+chmod -R 777 /var/www/html/storage
+chmod -R 777 /var/www/html/bootstrap/cache
+mkdir -p /var/www/html/storage/logs
+touch /var/www/html/storage/logs/laravel.log
+chmod 777 /var/www/html/storage/logs/laravel.log
+
 echo "Running package discover..."
 php artisan package:discover --ansi
 
